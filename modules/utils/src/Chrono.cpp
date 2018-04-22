4b91cfbbc58b643938cd9bf1e2d55ab7bd16aff5
Slava Melanko <vandernyt23@gmail.com>
2018-04-22 22:17:11 +0300

    Added foo test function to check AddressSanitizer

--- modules/utils/src/Chrono.cpp
+++ modules/utils/src/Chrono.cpp
@@ -8,46 +8,17 @@ namespace utils
 namespace chrono
 {
 
-int Global;
-
-void* Thread1(void* x)
-{
-	Global++;
-	return NULL;
-}
-
-void* Thread2(void* x)
-{
-	Global--;
-	return NULL;
+char foo() {
+    char *x = (char*)malloc(10 * sizeof(char*));
+    free(x);
+    return x[5];
 }
 
 std::string GetCurrentDateTime(const std::string& format)
 {
 	// AddressSanitizer
-	auto test = []() {
-		int *array = new int[100];
-		delete [] array;
-		return array[3]; // BOOM
-	}();
+	auto test = foo();
 
-	// ThreadSanitizer
-	[]() {
-		pthread_t t[2];
-		pthread_create(&t[0], NULL, Thread1, NULL);
-		pthread_create(&t[1], NULL, Thread2, NULL);
-		pthread_join(t[0], NULL);
-		pthread_join(t[1], NULL);
-	}();
-
-	// MemorySanitizer
-	[]() {
-		int* a = new int[10];
-		a[5] = 0;
-		if (a[2]) {
-			printf("xx\n");
-		}
-	}();
 	// Create a timestamp with the current time.
 	Poco::Timestamp now;
 
