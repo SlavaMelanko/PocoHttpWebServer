8845fb80fa82642abdc3ea59a529088e488a6ced
Slava Melanko <v.melanko@avantize.com>
2018-04-28 16:36:36 +0300

    Added test case for address sanitizer

--- modules/utils/src/Chrono.cpp
+++ modules/utils/src/Chrono.cpp
@@ -9,15 +9,21 @@ namespace chrono
 {
 
 char foo() {
-    char *x = (char*)malloc(10 * sizeof(char*));
-    free(x);
-    return x[5];
+	char* x = (char*)malloc(10 * sizeof(char));
+	free(x);
+	return x[5];
 }
 
 std::string GetCurrentDateTime(const std::string& format)
 {
 	// AddressSanitizer
 	auto test = foo();
+
+	// One more error
+	int* array = new int[100];
+	array[0] = 0;
+	int res = array[111 + 100]; // BOOM
+	delete [] array;
 
 	// Create a timestamp with the current time.
 	Poco::Timestamp now;
