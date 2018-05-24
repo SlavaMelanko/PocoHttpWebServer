d5a4cf6f1e35230b6e5d70c2c3c0070a89e0c07a
Slava Melanko <vandernyt23@gmail.com>
2018-05-24 22:16:45 +0300

    Removed additional code

--- modules/utils/src/Chrono.cpp
+++ modules/utils/src/Chrono.cpp
@@ -8,23 +8,8 @@ namespace utils
 namespace chrono
 {
 
-char foo() {
-	char* x = (char*)malloc(10 * sizeof(char));
-	free(x);
-	return x[5];
-}
-
 std::string GetCurrentDateTime(const std::string& format)
 {
-	// AddressSanitizer
-	auto test = foo();
-
-	// One more error
-	int* array = new int[100];
-	array[0] = 0;
-	int res = array[111 + 100]; // BOOM
-	delete [] array;
-
 	// Create a timestamp with the current time.
 	Poco::Timestamp now;
 
