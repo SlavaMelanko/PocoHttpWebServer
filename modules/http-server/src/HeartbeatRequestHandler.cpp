7810015b047b627b5723e37b7e425680fd26d93f
Slava Melanko <v.melanko@avantize.com>
2018-05-31 20:59:20 +0300

    Added Heartbeat request handler

---
+++ modules/http-server/src/HeartbeatRequestHandler.cpp
@@ -0,0 +1,22 @@
+#include "HeartbeatRequestHandler.h"
+
+#include <iostream>
+
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTTPServerResponse.h>
+#include <Poco/Util/ServerApplication.h>
+
+#include "Version.h"
+
+void HeartbeatRequestHandler::handleRequest(Poco::Net::HTTPServerRequest& request,
+	Poco::Net::HTTPServerResponse& response)
+{
+	auto& app = Poco::Util::Application::instance();
+	app.logger().information(
+		"Request from " + request.clientAddress().toString());
+
+	response.setContentType("text/html");
+
+	std::ostream& ostr = response.send();
+	ostr << "v" << APP_VERSION;
+}
---
+++ modules/http-server/src/HeartbeatRequestHandler.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <Poco/Net/HTTPRequestHandler.h>
+
+class HeartbeatRequestHandler
+	: public Poco::Net::HTTPRequestHandler
+{
+public:
+	/// Returns server version and verifies service health.
+	void handleRequest(Poco::Net::HTTPServerRequest& request,
+		Poco::Net::HTTPServerResponse& response) override;
+};
